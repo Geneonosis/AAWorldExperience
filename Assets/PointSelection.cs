3278b0d86b0ae7b98874e24afc0cf1c9e0415a2e
geneonosis <genetigner.art@gmail.com>
2019-11-09 20:08:03 -0600

    pointmapping
    point mapping

---
+++ Assets/PointSelection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointSelection : MonoBehaviour
+{
+    private string testString;
+
+    [SerializeField] Vector3 source;
+    [SerializeField] Vector3 destination;
+    [SerializeField] GameObject pointMarker;
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = Random.onUnitSphere * 3;
+        destination = Random.onUnitSphere * 3;
+
+        Instantiate<GameObject>(pointMarker,source,new Quaternion(0,0,0,0),GameObject.FindGameObjectWithTag("World").transform);
+        Instantiate<GameObject>(pointMarker,destination, new Quaternion(0, 0, 0, 0), GameObject.FindGameObjectWithTag("World").transform);
+    }
+}
