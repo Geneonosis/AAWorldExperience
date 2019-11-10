84b0025d74653bb2e79f6a64709fdc246c821c5f
geneonosis <genetigner.art@gmail.com>
2019-11-10 08:51:17 -0600

    airplanes
    and airplains

---
+++ Assets/cameraFlip.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFlip : MonoBehaviour
+{
+    Camera camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = this.gameObject.GetComponent<Camera>();
+        camera.projectionMatrix = camera.projectionMatrix * Matrix4x4.Scale(new Vector3(-1, 1, 1));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
